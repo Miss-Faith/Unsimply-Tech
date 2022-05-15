b39d1b89a504cf2c6cf8a75f3965731b6f4e7c17
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-15 15:43:14 +0300

    Add class Blog and comment

--- app/models.py
+++ app/models.py
@@ -31,8 +31,10 @@ class User(UserMixin,db.Model):
     profile_pic_path = db.Column(db.String())
     secure_password = db.Column(db.String(255),nullable = False)
 
-    quotes = db.relationship('Pitch', backref='user', lazy='dynamic')
+    blogs = db.relationship('Blog', backref='user', lazy='dynamic')
     comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
+
+    is_admin = db.Column(db.Boolean, default=False)
 
     @property
     def password(self):
@@ -55,3 +57,55 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255),nullable = False)
+    description = db.Column(db.String(255),nullable=False)
+
+    blog = db.Column(db.Text(), nullable = False)
+    comment = db.relationship('Comment',backref='blog',lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete_blog(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def get_blog(id):
+        blog = Blog.query.filter_by(id=id).first()
+
+        return blog
+
+    def __repr__(self):
+        return f'blog {self.blog}'
+
+class Comment(db.Model):
+
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text(),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
+    blog_id = db.Column(db.Integer,db.ForeignKey('blogs.id'),nullable = False)
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_comment(self):
+        db.session.remove(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,id):
+        comments = Comment.query.filter_by(id=id).all()
+        return comments
+
+    def __repr__(self):
+        return f'comment:{self.comment}'
