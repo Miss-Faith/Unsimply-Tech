b7d59c8a13742416966f4ffcf9a06a6c6189bf7e
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-15 17:50:11 +0300

    Add class role

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import os
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Role,Comment
+from app.models import *
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
