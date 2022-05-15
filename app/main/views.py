23d9a8dbffed2c05438779051ca3d7d6b202da5b
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-15 17:17:15 +0300

    create comment route

--- app/main/views.py
+++ app/main/views.py
@@ -40,3 +40,21 @@ def new_blog():
         
     return render_template('new_blog.html', form = form)
 
+@main.route('/comment/<int:blog_id>', methods = ['POST','GET'])
+@login_required
+def comment(blog_id):
+    form = CommentForm()
+    blog = Blog.query.get(blog_id)
+    all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data 
+        blog_id = blog_id
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(comment = comment,user_id = user_id,blog_id = blog_id)
+        new_comment.save_comment()
+
+        return redirect(url_for('.comment', blog_id = blog_id))
+
+    return render_template('comment.html', form =form, blog = blog,all_comments=all_comments)
+
+
