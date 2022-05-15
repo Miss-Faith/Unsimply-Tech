7d0f54aeadd5a3772fe2ce24003ce2c0a7b7baa0
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-15 13:37:46 +0300

    Update init file

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
-from flask import render_template
-from . import main
+from flask import render_template, redirect
+from app import app
 
 #views
 @main.route('/')
 def index():
-    '''
+  '''
     View root page function that returns the index page and its data
   '''
 
