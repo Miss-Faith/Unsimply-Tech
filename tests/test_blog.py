fe1e8bbaf949d4d50696a4cd72bccdafbe61abf3
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-17 00:45:43 +0300

    Update test files

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -1,24 +1,25 @@
 import unittest
-from app.models import Blog, User,
+from app.models import *
 from app import db
 
 
 class BlogModelTest(unittest.TestCase):
     def setUp(self):
-        self.user_charles = User(username='cha', password='chako', email='test@test.com')
-        self.new_blog = Blog(id=1, title='Test', content='This is a test blog', user_id=self.user_charles.id)
+        self.new_user = User(id = 12345,username='cha', password='chako', email='test@test.com')
+        self.new_blog = Blog(id=1, title='Test', description='ha ha', blog='This is a test blog', user_id=12345)
 
     def tearDown(self):
-        Blog.query.delete()
+        Blog.query.delete_blog()
         User.query.delete()
 
     def test_check_instance_variables(self):
         self.assertEquals(self.new_blog.title, 'Test')
-        self.assertEquals(self.new_blog.content, 'This is a test blog')
-        self.assertEquals(self.new_blog.user_id, self.user_charles.id)
+        self.assertEquals(self.new_blog.description, 'ha ha')
+        self.assertEquals(self.new_blog.blog, 'This is a test blog')
+        self.assertEquals(self.new_blog.user_id, 12345)
 
     def test_save_blog(self):
-        self.new_blog.save()
+        self.new_blog.save_blog()
         self.assertTrue(len(Blog.query.all()) > 0)
 
     def test_get_blog(self):
--- tests/test_comments.py
+++ tests/test_comments.py
@@ -6,7 +6,7 @@ class TestComment(unittest.TestCase):
 
     def setUp(self):
         self.user_James = User(id = 12345, username = 'James', email = 'james@ms.com')
-        self.new_comment = Comment(id=12345,comment='This pitch is the best thing since sliced bread',user_id = self.user_James,pitch_id=123)
+        self.new_comment = Comment(comment='This blog is the best thing since sliced bread',user_id = self.user_James,blog_id=123)
 
     def tearDown(self):
         Comment.query.delete()
@@ -16,10 +16,9 @@ class TestComment(unittest.TestCase):
         self.assertTrue(isinstance(self.new_comment,Comment))
 
     def test_check_instance_variables(self):
-        self.assertEquals(self.new_comment.id,12345)
-        self.assertEquals(self.new_comment.comment,'This pitch is the best thing since sliced bread')
+        self.assertEquals(self.new_comment.comment,'This blog is the best thing since sliced bread')
         self.assertEquals(self.new_comment.user_id,self.user_James)
-        self.assertEquals(self.new_comment.pitch_id,123)
+        self.assertEquals(self.new_comment.blog_id,123)
 
     def test_save_comment(self):
         self.new_comment.save_comment()
